cc59b68fe861f970774817b1ba549767d5e6a682
unknown <zeevhyorg@gmail.com>
2021-12-23 23:13:18 +0500

    Changes applied to database

--- Pentutor/settings.py
+++ Pentutor/settings.py
@@ -103,10 +103,18 @@ WSGI_APPLICATION = 'Pentutor.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # },
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME':  'pentutordb',
+        'USER' : 'huzaifa',
+        'PASSWORD' : 'PenTutor0004' ,
+        'HOST' : '31.187.74.98' ,
+        'PORT' : 5432
+    },
 }
 
 
