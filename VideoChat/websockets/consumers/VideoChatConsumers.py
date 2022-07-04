01dc1f35928af469ae5b54d0a2418c2343c6de73
HuzaifaYaqoob <zeevhyorg@gmail.com>
2022-07-04 09:57:25 +0500

    sockets for user and video chat

--- VideoChat/websockets/consumers/VideoChatConsumers.py
+++ VideoChat/websockets/consumers/VideoChatConsumers.py
@@ -11,6 +11,22 @@ class VideoChatConsumers(WebsocketConsumer):
         self.send('hellow')
 
     def receive(self, text_data=None, bytes_data=None):
+        print(text_data)
+        print('gonna receive')
+
+    def disconnect(self, code):
+        print('disconnected')
+
+
+class ActivatedVideoChat(WebsocketConsumer):
+
+    def connect(self):
+        print('gonna accept')
+        self.accept()
+        self.send('hellow')
+
+    def receive(self, text_data=None, bytes_data=None):
+        print(text_data)
         print('gonna receive')
 
     def disconnect(self, code):
