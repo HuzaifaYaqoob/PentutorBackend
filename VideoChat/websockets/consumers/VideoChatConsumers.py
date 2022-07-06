2e2d0942bb28b65b87ab9959788b6ab222564201
HuzaifaYaqoob <zeevhyorg@gmail.com>
2022-07-06 09:34:42 +0500

    active video chat sockets

--- VideoChat/websockets/consumers/VideoChatConsumers.py
+++ VideoChat/websockets/consumers/VideoChatConsumers.py
@@ -4,21 +4,46 @@ from channels.generic.websocket import WebsocketConsumer
 import json
 from asgiref.sync import async_to_sync
 
+from VideoChat.models import VideoChat
+from django.contrib.auth.models import User
 class VideoChatConsumers(WebsocketConsumer):
 
-        
+    # all types 
+    # CONNECTED
+    # NEW_CONNECTION_REQUEST
+    # NEW_CONNECTION_REQUEST
+    # CONNECTION_ACCEPTED
+
+    activated_vc_channel_base = 'active-video-chat-<videochat_id_here>'
     
     def connect(self):
         self.user = self.scope['user']
-        if self.user.is_authenticated:
-            self.video_chat_id = self.scope['url_route']['kwargs']['videochat_id']
+        self.video_chat_id = self.scope['url_route']['kwargs']['videochat_id']
+
+        try:
+            get_chat = VideoChat.objects.get(id=self.video_chat_id)
+        except:
+            get_chat = None
+        if self.user.is_authenticated and get_chat is not None:
+            self.vidChat = get_chat
             self.channel_base = f'video-chat-user-socket-{self.video_chat_id}-{self.user.username}'
             self.accept()
-            print(self.channel_base)
             async_to_sync(self.channel_layer.group_add)(
                 self.channel_base,
                 self.channel_name
             )
+
+            async_to_sync(self.channel_layer.group_send)(
+                self.channel_base,
+                {
+                    'type' : 'chat.message',
+                    'message' : {
+                        'type' : 'CONNECTED',
+                        'message' : 'Connected to Video chat'
+                    }
+                }
+            )
+
 
     def receive(self, text_data=None, bytes_data=None):
         text_data = json.loads(text_data)
@@ -26,7 +51,6 @@ class VideoChatConsumers(WebsocketConsumer):
 
         types_ = {
             'NEW_CONNECTION_REQUEST' : self.new_connection_request,
-            'NEW_CONNECTION_ACCEPTED' : self.new_connection_accepted
         }
 
         if r_type in types_:
@@ -36,36 +60,98 @@ class VideoChatConsumers(WebsocketConsumer):
     def disconnect(self, code):
         print('disconnected')
 
-
+    def chat_message(self, event):
+        self.send(json.dumps(event['message']))
 
     
     def new_connection_request(self):
-        print('NEW CONNECTION REQUEST RECEIEVE')
+        async_to_sync(self.channel_layer.group_send)(
+            f'active-video-chat-{self.video_chat_id}',
+            {
+                'type' : 'chat.message',
+                'message' : {
+                    'type' : 'NEW_CONNECTION_REQUEST',
+                    'user' : {
+                        'username' : self.user.username,
+                        'email' : self.user.email
+                    },
+                    'chat' : {
+                        'name' : self.vidChat.name,
+                        'host' : {
+                            'username' : self.vidChat.host.username,
+                            'email' : self.vidChat.host.email,
+                            'id' : self.vidChat.host.id,
+                        }
+                    },
+                    'message' : f'{self.username} want to join this meeting.'
+                }
+            }
+        )
     
-    def new_connection_accepted(self):
-        print('NEW CONNECTION ACCEPTED')
+
+
+
+
+
+
+
+
+
 
 
 class ActivatedVideoChat(WebsocketConsumer):
 
-    # def __init__(self):
-    #     self._types = {
-    #         'NEW_CONNECTED_ADDED' : self.new_connection_added
-    #     }
+    # NEW_CONNECTION_ACCEPTED
 
     def connect(self):
-        print('gonna accept')
-        self.accept()
-        self.send('hellow')
+        self.user = self.scope['user']
+        self.video_chat_id = self.scope['url_route']['kwargs']['videochat_id']
+
+        try:
+            get_chat = VideoChat.objects.get(id=self.video_chat_id)
+        except:
+            get_chat = None
+        if self.user.is_authenticated and get_chat is not None and self.user in get_chat.paticipants:
+            self.vidChat = get_chat
+            self.accept()
+            self.send('hellow')
 
     def receive(self, text_data=None, bytes_data=None):
-        print(text_data)
+        data = json.loads(text_data)
+        r_type = data['type']
+
+        if r_type == 'NEW_CONNECTION_ACCEPTED':
+            self.new_connection_accepted(data['message'])
         print('gonna receive')
 
     def disconnect(self, code):
         print('disconnected')
 
 
+    def chat_message(self, event):
+        message = event['message']
+
+        self.send(json.dumps(message))
+
+    def new_connection_accepted(self, message):
+        try:
+            get_user = User.objects.get(username=message.user.username, email=message.user.email)
+            self.vidChat.paticipants.add(get_user)
+            self.vidChat.save()
+        except Exception as err:
+            print(err)
+            # pass
+
+        async_to_sync(self.channel_layer.group_send)(
+            f'video-chat-user-socket-{self.video_chat_id}-{message.user.username}',
+            {
+                'type' : 'chat.message',
+                'message' : {
+                    'type' : 'CONNECTION_ACCEPTED',
+                    'message' : f'{self.user.username} has accepted to join you this meeting.'
+                }
+            }
+        )
 
 
     def new_connection_added(self):
