a7319f376f63bbfbab8169be93c31012e97fd37d
faisaltufail <rana.faisal998@gmail.com>
2022-08-28 00:25:43 +0500

    crud of course done

--- Course/urls.py
+++ Course/urls.py
@@ -6,5 +6,8 @@ from . import views
 
 urlpatterns = [
     path('get_all_courses/' , views.get_all_courses),
+    path('create_course/' , views.create_course),
+    path('delete_course/' , views.delete_course),
+    path('update_course/' , views.update_course),
     
 ]
