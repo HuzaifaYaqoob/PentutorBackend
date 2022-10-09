932b859d584724417209d01e8fc9664fbec98483
HuzaifaYaqoob <zeevhyorg@gmail.com>
2022-10-09 15:18:34 +0500

    job module started

--- Blog/urls.py
+++ Blog/urls.py
@@ -6,4 +6,5 @@ from . import views
 
 urlpatterns = [
     path('get_blog_posts/' , views.get_blog_posts),
+    path('get_single_blog/' , views.get_single_blog),
 ]
--- Blog/views.py
+++ Blog/views.py
@@ -18,3 +18,16 @@ def get_blog_posts(request):
     serialized = BlogPostSerializer(all_posts, many=True)
     return Response({'data' : serialized.data},status=status.HTTP_200_OK)
 
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def get_single_blog(request):
+    slug_id = request.GET.get('slug', None)
+    try:
+        post = BlogPost.objects.get(slug=slug_id)
+    except:
+        return Response({'message' : 'Not Found'}, status=status.HTTP_404_NOT_FOUND)
+
+    serialized = BlogPostSerializer(post)
+    return Response({'data' : serialized.data},status=status.HTTP_200_OK)
+
