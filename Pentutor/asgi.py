f7b79859147e174415f981197068f7d66538268c
HuzaifaYaqoob <zeevhyorg@gmail.com>
2024-06-09 23:36:57 +0500

    changes

--- Pentutor/asgi.py
+++ Pentutor/asgi.py
@@ -8,12 +8,9 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
-import django
-django.setup()
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Pentutor.settings')
-
 
 # application = get_asgi_application()
 
