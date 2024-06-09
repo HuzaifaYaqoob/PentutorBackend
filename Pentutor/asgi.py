c9510c72251c2a2009c69eee1f8fe0d820f303d7
HuzaifaYaqoob <zeevhyorg@gmail.com>
2024-06-10 00:30:10 +0500

    changes

--- Pentutor/asgi.py
+++ Pentutor/asgi.py
@@ -1,21 +1,33 @@
+"""
+ASGI config for Pentutor project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
 import os
 import django
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Pentutor.settings')
 
 django.setup()
+# application = get_asgi_application()
 
+
+from channels.routing import ProtocolTypeRouter, URLRouter
 import VideoChat.websockets.urls
 from .websocket.SocketMiddlewares import TokenAuthMiddleware
 
 
 application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
-    'websocket': TokenAuthMiddleware(
-        URLRouter(
-            VideoChat.websockets.urls.websocket_urls
+    'http' : get_asgi_application(),
+    'websocket' : TokenAuthMiddleware(
+            URLRouter(
+                VideoChat.websockets.urls.websocket_urls,
+            )
         )
-    ),
+
 })
