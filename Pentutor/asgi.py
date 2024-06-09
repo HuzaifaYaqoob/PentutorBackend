3f1e316effda5045860cb8345f094813a54cc44b
HuzaifaYaqoob <zeevhyorg@gmail.com>
2024-06-10 00:10:58 +0500

    changes

--- Pentutor/asgi.py
+++ Pentutor/asgi.py
@@ -2,12 +2,14 @@ import os
 import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-import VideoChat.websockets.urls
-from .websocket.SocketMiddlewares import TokenAuthMiddleware
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Pentutor.settings')
 
 django.setup()
+
+import VideoChat.websockets.urls
+from .websocket.SocketMiddlewares import TokenAuthMiddleware
+
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
