ab1c14740ae66e14cdc8ab9026d6db172ee2b649
HuzaifaYaqoob <zeevhyorg@gmail.com>
2024-06-09 20:20:16 +0500

    changes

--- Pentutor/settings.py
+++ Pentutor/settings.py
@@ -135,18 +135,18 @@ CHANNEL_LAYERS = {
 }
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # },
     'default': {
-        'ENGINE': env('DATABASE_ENGINE'),
-        'NAME':  env('DATABASE_NAME'),
-        'USER' : env('DATABASE_USER'),
-        'PASSWORD' : env('DATABASE_PASSWORD') ,
-        'HOST' : env('DATABASE_HOST') ,
-        'PORT' : env('DATABASE_PORT')
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     },
+    # 'default': {
+    #     'ENGINE': env('DATABASE_ENGINE'),
+    #     'NAME':  env('DATABASE_NAME'),
+    #     'USER' : env('DATABASE_USER'),
+    #     'PASSWORD' : env('DATABASE_PASSWORD') ,
+    #     'HOST' : env('DATABASE_HOST') ,
+    #     'PORT' : env('DATABASE_PORT')
+    # },
 }
 
 
