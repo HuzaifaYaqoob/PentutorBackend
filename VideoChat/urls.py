30425ada443a35d5944530047caca9cb4bfad2fb
HuzaifaYaqoob <zeevhyorg@gmail.com>
2024-07-02 15:12:52 +0500

    video chat

--- VideoChat/urls.py
+++ VideoChat/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
     path('create_video_chat/', views.create_video_chat),
     path('get_video_chat/', views.get_video_chat),
-    path('get_user_video_chats/', views.get_video_chat),
+    path('get_user_video_chats/', views.get_user_video_chats),
 
     # Demo Call 
     path('request-tutor-demo-class/', views.requestTutorDemoClass),
