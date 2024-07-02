b08022b0ded85bd3a9c3a5443c05c2d48e9bfc16
HuzaifaYaqoob <zeevhyorg@gmail.com>
2024-07-02 15:51:41 +0500

    changes

--- Pentutor/settings.py
+++ Pentutor/settings.py
@@ -189,14 +189,13 @@ REST_FRAMEWORK = {
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
+
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "Asia/Karachi"
 
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
