ce8a9c28bbf52952ec2d4e01cfc2230bccb1a07f
HuzaifaYaqoob <zeevhyorg@gmail.com>
2024-07-02 15:49:16 +0500

    changes

--- VideoChat/serializers.py
+++ VideoChat/serializers.py
@@ -5,6 +5,7 @@ from rest_framework import serializers
 from Authentication.serializers import UserSerializer
 
 from .models import VideoChat, VideoChatMedia, VideoChatSetting
+from datetime import datetime
 
 
 class SettingSerializer(serializers.ModelSerializer):
@@ -40,10 +41,20 @@ class VideoChatClasses(serializers.ModelSerializer):
     allowed_users = UserSerializer(many=True, read_only=True)
 
     day_name  = serializers.SerializerMethodField()
+    start_meeting  = serializers.SerializerMethodField()
 
     def get_day_name(self, obj):
         return obj.date.strftime("%A")
+
+
+    def get_start_meeting(self, obj):
+        time_now = datetime.now()
+        if obj.date == time_now.date():
+            if time_now.time() >= obj.start_time and time_now.time() <= obj.end_time:
+                return True
+        else:
+            return False
     
     class Meta:
         model = VideoChat
-        fields = ['id', 'host', 'allowed_users', 'day_name', 'start_time', 'end_time']
+        fields = ['id', 'host', 'allowed_users', 'day_name', 'start_time', 'end_time', 'start_meeting']
